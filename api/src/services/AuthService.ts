2fa3eb9c3b42e9661a5e987f3d6d7c6605cc484e
Morgane-Compa <morgane.compafloury@gmail.com>
2025-08-01 23:28:26 +0200

    [inscription-feature] : correction adress problem

--- api/src/services/AuthService.ts
+++ api/src/services/AuthService.ts
@@ -3,21 +3,29 @@ import { Adress } from "../entities/Adress";
 import { Member } from "../entities/Member";
 import { Residence } from "../entities/Residence";
 import bcrypt from "bcrypt";
+import { ResidenceService } from "./ResidenceService";
+
+type AddressInput = {
+    streetNumber: string;
+    streetName: string;
+    city: string;
+    postalCode: string;
+};
 
 export class AuthService {
 
     private authRepository = appDataSource.getRepository(Member);
     private residenceRepository = appDataSource.getRepository(Residence);
-    private adressRepository = appDataSource.getRepository(Adress);
+    private residenceService = new ResidenceService();
 
-
-    // Get all users
+    // Get all residence
+    //Get all users 
     async getAll() {
         console.log("UserService");
         return this.authRepository.find();
     };
 
-    // Get one user by id
+    //Get all users by id 
     async getById(id: number) {
         console.log("UserService");
         return this.authRepository.findOneBy({ id: id });
@@ -25,56 +33,45 @@ export class AuthService {
 
     // Création du premier syndic d'une copro
     async registerSyndic(data: {
-    residenceName: string;
-    addresses: { streetNumber: string; streetName: string; postalCode: string }[];
-    lastName: string;
-    firstName: string;
-    email: string;
-    password: string;
-    phoneNumber?: string;
-    age?: number;
-    appartmentNumber?: string;
-  }) {
+        residenceName: string;
+        addresses: AddressInput[];
+        lastName: string;
+        firstName: string;
+        email: string;
+        password: string;
+        phoneNumber?: string;
+        age?: number;
+        appartmentNumber?: string;
+    }) {
+        const residence = await this.getOrCreateResidence(data.residenceName, data.addresses);
+        await this.ensureEmailIsAvailable(data.email);
+        const hashedPassword = await this.hashPassword(data.password);
 
-    const residence = await this.getOrCreateResidence(data.residenceName, data.addresses);
-    await this.ensureEmailIsAvailable(data.email);
-    const hashedPassword = await this.hashPassword(data.password);
+        const newMember = this.authRepository.create({
+            lastName: data.lastName,
+            firstName: data.firstName,
+            email: data.email,
+            password: hashedPassword,
+            phoneNumber: data.phoneNumber,
+            age: data.age,
+            appartmentNumber: data.appartmentNumber,
+            isAdmin: true,
+            residence: residence,
+        });
 
-    const newMember = this.authRepository.create({
-      lastName: data.lastName,
-      firstName: data.firstName,
-      email: data.email,
-      password: hashedPassword,
-      phoneNumber: data.phoneNumber,
-      age: data.age,
-      appartmentNumber: data.appartmentNumber,
-      isAdmin: true,
-      residence: residence,
-    });
+        return this.authRepository.save(newMember);
+    }
 
-    return this.authRepository.save(newMember);
-  }
-
-    private async getOrCreateResidence(name: string, addresses: { streetNumber: string; streetName: string; postalCode: string }[]) {
+    private async getOrCreateResidence(name: string, addresses: AddressInput[]) {
         let residence = await this.residenceRepository.findOneBy({ name });
-
         if (!residence) {
-            // Création de la résidence
-            residence = this.residenceRepository.create({ name });
-            await this.residenceRepository.save(residence);
+            const newResidence = this.residenceRepository.create({
+                name,
+                adresses: addresses,
+            });
 
-            // Création des adresses liées
-            for (const addr of addresses) {
-                const newAddress = this.adressRepository.create({
-                    streetNumber: addr.streetNumber,
-                    streetName: addr.streetName,
-                    postalCode: addr.postalCode,
-                    residence: residence
-                });
-                await this.adressRepository.save(newAddress);
-            }
+            residence = await this.residenceService.create(newResidence);
         }
-
         return residence;
     }
 
