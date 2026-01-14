6d2cdf474b7c5e47719203ee35a30b912aee67ce
Morgane-Compa <morgane.compafloury@gmail.com>
2026-01-14 17:24:22 +0100

    [fix-first-pipeline-problems] : correction des erreurs de pipelines

--- app/src/contexts/auth.context.ts
+++ app/src/contexts/auth.context.ts
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import { createContext } from "react";
 import type { AuthContextValue } from "./auth.types";
 
 export const AuthContext = createContext<AuthContextValue | null>(null);
